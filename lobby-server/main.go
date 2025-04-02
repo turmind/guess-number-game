bfc4299e87d9d65ca1f692c13ad789fa09939d03
jdhuang <jdhuang@amazon.com>
2025-04-02 21:49:42 +0800

    simplify lobby-server

--- lobby-server/main.go
+++ lobby-server/main.go
@@ -8,51 +8,43 @@ import (
 	"net/http"
 )
 
+const (
+	defaultPort = 8080
+)
+
 var battleServerURL string
 
-type MatchResponse struct {
-	WsUrl string `json:"wsUrl"`
-}
-
-func handleMatch(w http.ResponseWriter, r *http.Request) {
-	// 设置CORS头
+func setCORS(w http.ResponseWriter) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+}
 
-	// 处理预检请求
+func match(w http.ResponseWriter, r *http.Request) {
+	setCORS(w)
+
 	if r.Method == "OPTIONS" {
-		w.WriteHeader(http.StatusOK)
 		return
 	}
 
-	// 只允许GET请求
 	if r.Method != "GET" {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
-	// 返回战斗服务器地址
 	w.Header().Set("Content-Type", "application/json")
-	response := MatchResponse{
-		WsUrl: battleServerURL,
-	}
-	if err := json.NewEncoder(w).Encode(response); err != nil {
-		log.Printf("Error encoding response: %v", err)
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		return
-	}
+	json.NewEncoder(w).Encode(map[string]string{"wsUrl": battleServerURL})
 }
 
 func main() {
-	port := flag.Int("port", 8080, "Port to run the lobby server on")
-	battlePort := flag.Int("battle-port", 8081, "Port of the battle server")
+	port := flag.Int("port", defaultPort, "Port to run the lobby server on")
 	flag.Parse()
 
-	battleServerURL = fmt.Sprintf("ws://localhost:%d/game", *battlePort)
-
-	http.HandleFunc("/match", handleMatch)
+	battleServerURL = fmt.Sprintf("ws://localhost:8081/game")
 	addr := fmt.Sprintf(":%d", *port)
+
+	http.HandleFunc("/match", match)
+
 	log.Printf("Lobby server starting on %s", addr)
 	if err := http.ListenAndServe(addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
